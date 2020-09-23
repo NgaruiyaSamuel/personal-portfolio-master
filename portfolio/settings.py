c4c5c03406438c5af6941669caca7bb02cd0314a
samkit47 <45119202+samkit47@users.noreply.github.com>
2020-09-23 19:29:23 +0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -135,3 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
