f15694903f377d5bed8d90a9afc5fa2cc8f5777a
samkit47 <45119202+samkit47@users.noreply.github.com>
2020-09-23 19:20:41 +0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
