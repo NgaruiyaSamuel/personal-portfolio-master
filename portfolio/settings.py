514b9ff30cbec270aa60231f71432baba84cca3b
samkit47 <45119202+samkit47@users.noreply.github.com>
2020-09-23 19:24:29 +0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,3 +124,13 @@ STATIC_URL = '/static/'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
+
